6cab684f1dce8380cdf3a9e9731ce0a39d334fcf
Ronny <60224237+ronny-ellis@users.noreply.github.com>
2025-02-10 03:26:31 +0300

    Create Version20250210002512.php

---
+++ migrations/Version20250210002512.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250210002512 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commande ADD quantite INT NOT NULL, ADD est_termine TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commande DROP quantite, DROP est_termine');
+    }
+}
